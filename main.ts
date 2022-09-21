e5a1ba18afe66a26e550edfd6363c311ff85a207
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-21 16:54:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,23 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(500)
     }
     basic.pause(500)
+    sprite.delete()
+    sprite = game.createSprite(4, 4)
+    for (let index = 0; index < 4; index++) {
+        sprite.turn(Direction.Left, 90)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+    }
 })
 let sprite: game.LedSprite = null
 basic.showIcon(IconNames.Heart)
-sprite = game.createSprite(2, 3)
+sprite = game.createSprite(3, 3)
 basic.forever(function () {
 	
 })
